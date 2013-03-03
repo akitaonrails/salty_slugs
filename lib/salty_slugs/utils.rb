4e2a39567c01c4d2c7032d657abe9c2b5523bcb0
AkitaOnRails <fabioakita@gmail.com>
2013-03-03 13:08:19 -0300

    Replacing Iconv for String#encode to be Ruby 2.0 compatible

--- lib/salty_slugs/utils.rb
+++ lib/salty_slugs/utils.rb
@@ -2,14 +2,19 @@
 begin
   require "unicode"
 rescue LoadError
+  # do nothing
+end
+begin
   require "iconv"
+rescue LoadError
+  # do nothing
 end
 
 module SaltySlugs
   module Utils
     def self.sluggify(text)
       return nil if text.blank?
-      
+
       if defined?(Transliteration)
       	str = Transliteration.transliterate(text)
       	str = str.gsub(/\W+/, '-').gsub(/^-+/,'').gsub(/-+$/,'').downcase
@@ -18,8 +23,15 @@ module SaltySlugs
         str = Unicode.normalize_KD(text).gsub(/[^\x00-\x7F]/n,'')
         str = str.gsub(/\W+/, '-').gsub(/^-+/,'').gsub(/-+$/,'').downcase
         return str
-      else
+      elsif defined?(Iconv)
         str = Iconv.iconv('ascii//translit//ignore', 'utf-8', text).to_s
+        str.gsub!(/\W+/, ' ')
+        str.strip!
+        str.downcase!
+        str.gsub!(/\ +/, '-')
+        return str
+      else String.respond_to?(:encode)
+        str = text.force_encoding('UTF-8').encode('UTF-8', :invalid => :replace, :replace => '').encode('UTF-8')
         str.gsub!(/\W+/, ' ')
         str.strip!
         str.downcase!
